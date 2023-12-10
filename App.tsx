2af849d2c486eeeb4b120f20516ad7afd5c5f710
Rohan Udhwani <rohanudhwani2@gmail.com>
2023-12-10 18:30:03 +0530

    Added packages

--- App.tsx
+++ App.tsx
@@ -11,19 +11,21 @@ import PaymentScreen from './src/screens/PaymentScreen'
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Details"
-        component={DetailsScreen}
-        options={{animation:'slide_from_bottom'}}></Stack.Screen>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{animation:'slide_from_bottom'}}></Stack.Screen>
       
-      <Stack.Screen
-        name="Payment"
-        component={PaymentScreen}
-        options={{animation:'slide_from_bottom'}}></Stack.Screen>
-    </Stack.Navigator>
-  </NavigationContainer>
+        <Stack.Screen
+          name="Payment"
+          component={PaymentScreen}
+          options={{animation:'slide_from_bottom'}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 
--- android/app/src/main/java/com/coffee_shop_app/MainApplication.java
+++ android/app/src/main/java/com/coffee_shop_app/MainApplication.java
@@ -10,6 +10,9 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -25,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new ReanimatedPackage());
+          packages.add(new RNScreensPackage());
+
           return packages;
         }
 
