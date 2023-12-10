ce9606e7942a62779e5547627be2241226e9d154
Rohan Udhwani <rohanudhwani2@gmail.com>
2023-12-10 16:05:31 +0530

    Configured files

---
+++ src/components/CustomIcon.ts
@@ -0,0 +1,8 @@
+import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
+import icoMoonConfig from '../../selection.json';
+export default createIconSetFromIcoMoon(icoMoonConfig);
+// export const CustomIcon = createIconSetFromIcoMoon(
+//     icoMoonConfig,
+//     'Icomoon',
+//     'Icomoon.ttf',
+// );
